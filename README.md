59096a3b9ee0f25603c69468bc3b95aefc9d39eb
isAlvarez <157077804+isAlvarezDev@users.noreply.github.com>
2024-10-18 14:39:55 -0400

    Temperature converter project added

--- README.md
+++ README.md
@@ -19,3 +19,4 @@ Project ideas to take you from beginner to advanced in Frontend
 - [Custom Dropdown](https://roadmap.sh/projects/custom-dropdown)
 - [Task Tracker](https://roadmap.sh/projects/task-tracker-js)
 - [GitHub Random Repository](https://roadmap.sh/projects/github-random-repo)
+- [Temperature Converter](https://roadmap.sh/projects/temperature-converter)
---
+++ frontend-projects/18-temperature-converter/script.js
@@ -0,0 +1,49 @@
+const inputTemperature = document.querySelector(".input-temperature")
+let currentTemperature
+const selectFromUnit = document.getElementById("from-unit")
+const selectToUnit = document.getElementById("to-unit")
+const buttonConverter = document.querySelector(".btn-convert")
+buttonConverter.disabled = true
+const paragraphResult = document.createElement("p")
+
+function areAllFieldsFilledIn() {
+    if(inputTemperature.value !== "" && selectFromUnit.selectedIndex !== 0 && selectToUnit.selectedIndex !== 0) buttonConverter.disabled = false
+    else buttonConverter.disabled = true
+}
+
+inputTemperature.addEventListener("input", event => {
+    currentTemperature = +event.target.value
+    areAllFieldsFilledIn()
+})
+selectFromUnit.addEventListener("input", areAllFieldsFilledIn)
+selectToUnit.addEventListener("input", areAllFieldsFilledIn)
+
+function calculateTemperature(fromUnit, toUnit) {
+    let result
+    if(fromUnit === "Fahrenheit" && toUnit === "Celseus") {
+        result = (currentTemperature - 32) * 5/9
+    } else if(fromUnit === "Fahrenheit" && toUnit === "Kelvin") {
+        result = (currentTemperature - 32) * 5/9 + 273.15
+    } else if(fromUnit === "Celseus" && toUnit === "Fahrenheit") {
+        result = (currentTemperature * 9/5) + 32
+    } else if(fromUnit === "Celseus" && toUnit === "Kelvin") {
+        result = currentTemperature + 273.15
+    } else if(fromUnit === "Kelvin" && toUnit === "Celseus") {
+        result = currentTemperature - 273.15
+    } else if(fromUnit === "Kelvin" && toUnit === "Fahrenheit") {
+        result = (currentTemperature - 273.15) * 9/5 + 32
+    } else {
+        result = currentTemperature
+    }
+    return result.toFixed(2)
+}
+
+function showAnswer(result) {
+    paragraphResult.textContent = `${currentTemperature} ${selectFromUnit.value} is ${result} ${selectToUnit.value}`
+    paragraphResult.className = "temperature-result"
+    document.body.insertAdjacentElement("beforeend", paragraphResult)
+}
+
+buttonConverter.addEventListener("click", () => {
+    showAnswer(calculateTemperature(selectFromUnit.value, selectToUnit.value))
+})
